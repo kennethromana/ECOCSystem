d451fc5d8e99b6f3f7a515551cc800bf3ba922ec
kennethromana <romanamarkkenneth@gmail.com>
2023-05-31 12:59:27 +0800

    UPDATE

--- ECOCSystem/Controllers/UserController.cs
+++ ECOCSystem/Controllers/UserController.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ECOCSystem.Model;
 
 namespace ECOCSystem.Controllers
 {
     public class UserController : Controller
-    {
+    {   ECOCEntities db = new ECOCEntities();
         // GET: User
         public ActionResult Index()
         {
+          
             return View();
         }
 
         public ActionResult Login()
         {
+            var acc = db.Account.Where(o => o.Active == true).ToList();
             return View();
         }
     }
