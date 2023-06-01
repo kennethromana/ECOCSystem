67992620d873e4d82c226e499b540f3cd2979a24
kennethromana <romanamarkkenneth@gmail.com>
2023-06-01 16:35:18 +0800

    ADD CPTL MODEL

---
+++ ECOCSystem/Model/CPTLModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECOCSystem.Model
+{
+    public class CPTLModel
+    {
+        public int CPTLID { get; set; }
+        public string COCNumber { get; set; }
+        public string PolicyNumber { get; set; }
+        public Nullable<System.DateTime> EffectivityDate { get; set; }
+        public Nullable<System.DateTime> ExpiryDate { get; set; }
+        public bool isAuthenticated { get; set; }
+        public string AuthenticatedNumber { get; set; }
+        public Nullable<int> AuthenticatedDate { get; set; }
+        public string AgentCode { get; set; }
+        public Nullable<System.DateTime> SubmittedDate { get; set; }
+        public string PolicyStatus { get; set; }
+        public string CRName { get; set; }
+        public string CRContentType { get; set; }
+        public Nullable<int> CompanyID { get; set; }
+        public Nullable<int> CompanyBranchID { get; set; }
+
+    }
+}
