4acc2903bc4f6646c1368665481ff47a316d084e
kennethromana <romanamarkkenneth@gmail.com>
2023-06-01 15:13:53 +0800

    UPDATE
    ADD CPTL CONTROLLER

---
+++ ECOCSystem/Controllers/CPTLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECOCSystem.Controllers
+{
+    public class CPTLController : Controller
+    {
+        // GET: CPTL
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
