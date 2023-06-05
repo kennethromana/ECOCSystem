fabdaa1dde409d0c26b29faba29ad00c37cf6e88
kennethromana <romanamarkkenneth@gmail.com>
2023-06-05 14:33:10 +0800

    ADD UI FOR CLIENT

---
+++ ECOCSystem/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECOCSystem.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ClientRegistrationForm()
+        {
+            return View();
+        }
+    }
+}
