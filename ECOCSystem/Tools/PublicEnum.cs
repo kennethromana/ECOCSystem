b582ce7d14f16ec86482cf2be0cd8750cfb6c18c
kennethromana <romanamarkkenneth@gmail.com>
2023-06-08 18:12:16 +0800

    update
    add function  when selecting title type to client

---
+++ ECOCSystem/Tools/PublicEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECOCSystem.Tools
+{
+    public enum TitleTypeEnum
+    {
+        Individual = 1,
+        Corporate = 2,
+        CorporateWithAssignee = 3
+    }
+}
