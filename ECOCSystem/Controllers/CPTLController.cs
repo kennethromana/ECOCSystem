e8253d015bc233a8c6a9266e617afd5f34708f73
kennethromana <romanamarkkenneth@gmail.com>
2023-06-14 13:18:42 +0800

    Update
    ADD SUBMIT FORM FUNCTION

--- ECOCSystem/Controllers/CPTLController.cs
+++ ECOCSystem/Controllers/CPTLController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using ECOCSystem.Model;
@@ -15,11 +16,46 @@ namespace ECOCSystem.Controllers
             CPTLModel Model = new CPTLModel();
             return View(Model);
         }
-        public ActionResult CPTL(CPTLModel model)
+        [HttpPost]
+        public ActionResult CPTL(CPTLModel model, string submitType)
         {
+            var Status = "Error";
+            var PartialViewDataString = "";
+            var Message = "";
+            int MVID = model.CPTLID;
+            int? VehicleBodyTypeID = 0;
+            int? VehicleTypeID = 0;
 
-            //return PartialView("_Client", model);
-            return RedirectToAction("Index");
+            using (var db = new ECOCEntities())
+            using (var dbTransaction = db.Database.BeginTransaction())
+            {
+
+                try
+                {
+                    switch (submitType) 
+                    {
+                        case "CLIENT":
+                            {
+                                var clientModel = model.Client;
+
+                                Message = "Message: There's something wrong. Please contact Databridge support.";
+                            }
+                            break;
+                        default:
+                            Message = "Message: ErrorThere's something error. Please try again later";
+                            break;
+                    }
+                }
+                catch (Exception) 
+                {
+                    dbTransaction.Rollback();
+                    Message = "Message: There's something wrong. Please contact Databridge support.";
+                }
+            }
+
+            var jsonResult = Json(new { Status, Message, Data = PartialViewDataString, MVID });
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
         }
 
         public ActionResult CPTLList()
