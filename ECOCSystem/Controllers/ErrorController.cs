e58bed5f5cdd49d5bb99a58683da5b50c4d869d4
kennethromana <romanamarkkenneth@gmail.com>
2023-06-24 13:03:37 +0800

    Update error page add auto redirect to web config

---
+++ ECOCSystem/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using ECOCSystem.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECOCSystem.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        [AuthorizeUser]
+        public ActionResult Index()
+        {
+            return View("Error");   
+        }
+    }
+}
