ffa6a0681954c03006f1f83a9e34d556aa88ebef
kennethromana <romanamarkkenneth@gmail.com>
2023-06-26 18:32:23 +0800

    Add ecoc_api controller

---
+++ ECOCSystem/Controllers/ECOC_APIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using ECOCSystem.Model;
+using ECOCSystem.Tools;
+
+namespace ECOCSystem.Controllers
+{
+    public class ECOC_APIController : Controller
+    {
+        ECOCEntities db = new ECOCEntities();
+
+        HttpClient ECOC_Client = new HttpClient();
+        Uri URI;
+        UriBuilder builder = new UriBuilder();
+        NameValueCollection query = new NameValueCollection();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
