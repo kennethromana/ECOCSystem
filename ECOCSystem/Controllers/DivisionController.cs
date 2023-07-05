f62ffa8e93c241c170dff33f058cd35c4b1ae1d2
kennethromana <romanamarkkenneth@gmail.com>
2023-07-05 13:54:02 +0800

    Add Division
    Add Division Model/View/Controller

---
+++ ECOCSystem/Controllers/DivisionController.cs
@@ -0,0 +1,21 @@
+﻿using ECOCSystem.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECOCSystem.Model;
+
+namespace ECOCSystem.Controllers
+{
+    [AuthorizeUser]
+    public class DivisionController : Controller
+    {
+        // GET: Division
+        public ActionResult Index()
+        {
+            var model = new DivisionModel();
+            return View(model);
+        }
+    }
+}
---
+++ ECOCSystem/Model/DivisionModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECOCSystem.Model
+{
+    public class DivisionModel
+    {
+        public DivisionModel() 
+        {
+            Region = new RegionModel();
+            Province = new ProvinceModel();
+            City = new CityModel();
+        }
+        public int SelectedRegionID { get; set; }
+        public RegionModel Region { get; set; }
+        public ProvinceModel Province { get; set; }
+        public CityModel City { get; set; }
+    }
+    public class RegionModel
+    {
+        public int RegionID { get; set; }
+        public string RegionNumber { get; set; }
+        public string RegionName { get; set; }
+
+    }
+    public class ProvinceModel
+    {
+        public int ProvinceID { get; set; }
+        public int RegionID { get; set; }
+        public string ProvinceNumber { get; set; }
+        public string ProvinceName { get; set; }
+
+    }
+    public class CityModel
+    {
+
+        public int CityID { get; set; }
+        public int ProvinceID { get; set; }
+        public string CityNumber { get; set; }
+        public string CityName { get; set; }
+
+    }
+}
