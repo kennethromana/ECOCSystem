3bffe6da91c189debb63ce796a31fb01f63e75f6
kennethromana <romanamarkkenneth@gmail.com>
2023-07-05 14:02:54 +0800

    Update Division View

--- ECOCSystem/Controllers/DivisionController.cs
+++ ECOCSystem/Controllers/DivisionController.cs
@@ -12,10 +12,32 @@ namespace ECOCSystem.Controllers
     public class DivisionController : Controller
     {
         // GET: Division
+        ECOCEntities db = new ECOCEntities();
         public ActionResult Index()
         {
             var model = new DivisionModel();
             return View(model);
+        }
+        public ActionResult GetRegionList() 
+        {
+            try
+            {
+                //Creating instance of DatabaseContext class  
+                using (var db = new ECOCEntities())
+                {
+
+                    //select all insurance comapny exclude databridge asia which has ID = 1
+                    var tableData = db.Region.Where(o => o.Active).ToList();
+
+
+                    //Returning Json Data    
+                    return Json(new { data = tableData }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
