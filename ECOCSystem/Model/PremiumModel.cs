02d69cb0028d221b930a89b9e6dbb4d0739a0581
kennethromana <romanamarkkenneth@gmail.com>
2023-07-09 15:00:04 +0800

    Add Premium Settings View and function

---
+++ ECOCSystem/Model/PremiumModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECOCSystem.Model
+{
+    public class PremiumModel
+    {
+        public PremiumModel() 
+        {
+
+        }
+        public int PremiumID { get; set; }
+
+    }
+}
